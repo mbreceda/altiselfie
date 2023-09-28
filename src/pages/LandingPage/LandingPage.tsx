6ff00491a0f533a5dcabb550e1e53df838d40083
Miguel Breceda de Lara <mbreceda@altimetrik.com>
2023-09-27 19:26:50 -0600

    Update LandingPage component

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,4 +1,11 @@
-import { VStack, Container, Box, useMultiStyleConfig } from "@chakra-ui/react";
+import {
+  VStack,
+  Container,
+  Box,
+  Text,
+  useMultiStyleConfig,
+} from "@chakra-ui/react";
+// import { Icon } from "../../components/Icon";
 
 export default function AltiselfiLandigPage() {
   const styles = useMultiStyleConfig("MainContainer", {});
@@ -6,7 +13,14 @@ export default function AltiselfiLandigPage() {
   return (
     <Container sx={styles.main_container}>
       <VStack justifyContent="center" h="100%">
-        <Box>Hello</Box>
+        <Box>
+          <Text sx={styles.display_text}>Welcome to the</Text>
+          <Text sx={styles.display_text}>Profile Pic Style, Altimetrian!</Text>
+          <Text sx={styles.kickoff_text}>
+            Please, select image you'd like AI to spice up.
+          </Text>
+          {/* <Icon name="attach_file_add" color="black" /> */}
+        </Box>
       </VStack>
     </Container>
   );
--- src/theme/components/main-container.ts
+++ src/theme/components/main-container.ts
@@ -1,10 +1,21 @@
 import { ComponentMultiStyleConfig } from "@chakra-ui/react";
 
 const MainContainer: ComponentMultiStyleConfig = {
-  parts: ["main_container"],
+  parts: ["main_container", "display_text", "kickoff_text"],
   baseStyle: {
     main_container: {
       height: "calc(100vh)",
+      maxW: "1024px",
+    },
+    display_text: {
+      fontSize: "display.large",
+      fontWeight: 500,
+      textAlign: "center",
+    },
+    kickoff_text: {
+      fontSize: "headings.4",
+      fontWeight: 500,
+      textAlign: "center",
     },
   },
 };
