97767a7e677cc01e0a2fc6505349317208c3d242
Miguel Breceda de Lara <mbreceda@altimetrik.com>
2023-10-02 20:04:42 -0600

    Turn useFilter from a hook to a resuducer and add image backgrounds

--- src/hooks/useFileUpload.tsx
+++ src/hooks/useFileUpload.tsx
@@ -1,48 +1,74 @@
-import {
-  createContext,
-  useContext,
-  ChangeEvent,
-  useState,
-  useEffect,
-} from "react";
+import { createContext, useContext, useReducer, Dispatch } from "react";
 
-function useUploader() {
-  let url = "";
-  const [file, setFile] = useState(url);
-
-  useEffect(() => {
-    setFile(url);
-  }, [url]);
-
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    if (event && event?.target.files) {
-      url = URL.createObjectURL(event.target.files[0]);
-      setFile(url);
-    }
-  }
-
-  return {
-    file,
-    handleChange,
-  };
+export enum BackgroundActions {
+  UPDATE_BACKGROUND = "UPDATE_BACKGROUND",
 }
 
+type BackgroundType = {
+  value: string;
+  label: string;
+  description: string;
+  href: string;
+  selected: boolean;
+};
+
+type State = {
+  backgrounds: BackgroundType[];
+};
+
+type SetBackground = {
+  type: typeof BackgroundActions.UPDATE_BACKGROUND;
+  option: BackgroundType;
+};
+
+type BackgroundActionTypes = SetBackground;
+
+const initialState: State = {
+  backgrounds: [
+    {
+      value: "true_altimetrian",
+      label: "True Altimetran",
+      href: "/src/assets/images/true_altimetrian.png",
+      description:
+        "The profile pic of someone fully immersed in the Altiverse. Portraying honor and trust, it’s said that having it as a profile pic can boost project performance and overall professional results.",
+      selected: true,
+    },
+    {
+      value: "viva_mexico",
+      label: "¡Viva Mexico!",
+      href: "/src/assets/images/viva_mexico.png",
+      description:
+        "The profile pic of someone fully immersed in the Altiverse. Portraying honor and trust, it’s said that having it as a profile pic can boost project performance and overall professional results.",
+      selected: false,
+    },
+  ],
+};
+
+const reducer = (state: State, action: BackgroundActionTypes): State => {
+  switch (action.type) {
+    case BackgroundActions.UPDATE_BACKGROUND:
+      return {
+        ...state,
+        backgrounds: [...state.backgrounds],
+      };
+    default:
+      return state;
+  }
+};
+
+const BackgroundReducer = () => useReducer(reducer, initialState);
 interface iFileUpload {
   children: React.ReactNode;
 }
 
-const FileUploadContext = createContext<{
-  file: string;
-  handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
-}>({
-  file: "",
-  handleChange: () => {},
-});
+const FileUploadContext = createContext<
+  [State, Dispatch<BackgroundActionTypes>]
+>([{ backgrounds: [] }, () => {}]);
 
 const FileUploadProvider = ({ children }: iFileUpload) => {
-  const { file, handleChange } = useUploader();
+  const [state, dispatch] = BackgroundReducer();
   return (
-    <FileUploadContext.Provider value={{ file, handleChange }}>
+    <FileUploadContext.Provider value={[state, dispatch]}>
       {children}
     </FileUploadContext.Provider>
   );
